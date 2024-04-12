961b05457a176bccbe9b119bbd3c6e288e3fd1ad
tMinh <tminhcit@gmail.com>
2024-04-12 11:15:56 +0700

    New packet

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Christoc.Modules.TuanMVC.Components;
+using Christoc.Modules.TuanMVC.Models;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework.JavaScriptLibraries;
+
+namespace Christoc.Modules.TuanMVC.Controllers
+{
+    [DnnHandleError]
+    public class DemoController : DnnController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
